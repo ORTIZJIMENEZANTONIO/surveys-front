205f0261d985351049bddf490cf31b6c0af06686
Antonio Ortiz <antonio@axend.io>
2025-11-29 15:58:06 -0600

    nex.config updated to use vercel

--- next.config.js
+++ next.config.js
@@ -2,16 +2,12 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
+  images: {
+    unoptimized: false,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  experimental: {
-    appDir: false,
-  },
-  basePath: "/surveys-front",
-  assetPrefix: "/surveys-front/",
 };
 
 module.exports = nextConfig;
